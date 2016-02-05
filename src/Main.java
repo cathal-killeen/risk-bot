8eaf410bd784ebee8b2851e24796bf05dd4c8b95
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-05 13:38:20 +0000

    added main function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 /**
- * Created by Cathal on 05/02/16.
+ * SuckyBeigeFish
+ *  Cathal Killeen: 14300066
+ *  Stan O'Neill: 14368166
+ *  Adam Doran:
  */
 public class Main {
+    public static void main(String[] args){
+        
+    }
 }
