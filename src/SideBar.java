9437ee2c722e645aa84964bd6f8e34c7e23c2f47
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-10 14:01:32 +0000

    Added event listener to command box - enterkey console logs

--- src/SideBar.java
+++ src/SideBar.java
@@ -2,6 +2,7 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  * Created by Cathal on 09/02/16.
@@ -18,17 +19,34 @@ public class SideBar extends JPanel {
     }
 }
 
+// Command prompt class contains:
+//      - promptLabel: to prompt user what to do next
+//      - commandField: to take user input
 class CommandPrompt extends JPanel{
+    public static JLabel promptLabel = new JLabel("Welcome to RISK!");
+    public static JTextField commandField = new JTextField("Enter command here", 10);
+
     public CommandPrompt(){
         setLayout(new BorderLayout());
-        JLabel promptLabel = new JLabel("Welcome to RISK!");
+
+        //Prompt label - tells user current state/prompts what to do
         promptLabel.setBorder(new EmptyBorder(10,10,10,10));
         promptLabel.setBackground(Color.GRAY);
         promptLabel.setOpaque(true);
-        JTextField commandField = new JTextField("Enter command here", 10);
+
+        //prompt text field - allows user input
+        commandField.addActionListener(enterKeyPressed);
 
 
         add(promptLabel, BorderLayout.NORTH);
         add(commandField, BorderLayout.SOUTH);
     }
+
+    //listener for when user presses enter key in commandField
+    private Action enterKeyPressed = new AbstractAction(){
+        @Override
+        public void actionPerformed(ActionEvent e){
+            System.out.println(commandField.getText());
+        }
+    };
 }
