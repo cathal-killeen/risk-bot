d7d2a7c1f5d466c58992778564e39b941764693f
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-10 13:54:24 +0000

    Updated command box UI - added prompt label

--- src/SideBar.java
+++ src/SideBar.java
@@ -1,20 +1,34 @@
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
  * Created by Cathal on 09/02/16.
  */
 public class SideBar extends JPanel {
+    public static CommandPrompt commandPrompt = new CommandPrompt();
+
     public SideBar(){
         super();
         setPreferredSize(Constants.SIDEBAR_DIM);
         setLayout(new BorderLayout());
 
-        String message = "Enter commdands here";
+        add(commandPrompt, BorderLayout.SOUTH);
+    }
+}
 
+class CommandPrompt extends JPanel{
+    public CommandPrompt(){
+        setLayout(new BorderLayout());
+        JLabel promptLabel = new JLabel("Welcome to RISK!");
+        promptLabel.setBorder(new EmptyBorder(10,10,10,10));
+        promptLabel.setBackground(Color.GRAY);
+        promptLabel.setOpaque(true);
         JTextField commandField = new JTextField("Enter command here", 10);
 
+
+        add(promptLabel, BorderLayout.NORTH);
         add(commandField, BorderLayout.SOUTH);
     }
 }
