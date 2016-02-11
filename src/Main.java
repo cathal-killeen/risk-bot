c00d9252f48d553eaad657aa377eba71723a1f9b
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-11 15:37:43 +0000

    Converted countries array to arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * SuckyBeigeFish
@@ -10,11 +11,13 @@ import java.awt.*;
  */
 public class Main {
 
-    //contains all UI components
-    static JFrame gameFrame = new GameFrame();
-
     //array of all country objects
-    static Country[] countries = createCountries();
+    public static ArrayList<Country> countries = createCountries();
+
+    //contains all UI components
+    public static JFrame gameFrame = new GameFrame();
+
+
 
     public static void main(String[] args){
 
@@ -23,19 +26,19 @@ public class Main {
         //TESTS
 
         //console log all countries
-        for(int i = 0; i < countries.length; i++){
-            System.out.println(countries[i].toString());
+        for(int i = 0; i < countries.size(); i++){
+            System.out.println(countries.get(i).toString());
         }
 
     }
 
     // create all country objects -- possibly relocate to another file
     // NOTE: does not put country nodes on map, but Point2D coOrds can be accessed from each object
-    public static Country[] createCountries(){
-        Country[] countries = new Country[Constants.NUM_COUNTRIES];
+    public static ArrayList createCountries(){
+        ArrayList<Country> countries = new ArrayList<Country>();
 
-        for(int i = 0; i < countries.length; i++){
-            countries[i] = new Country(i);
+        for(int i = 0; i < Constants.NUM_COUNTRIES; i++){
+            countries.add(new Country(i));
         }
 
         return countries;
--- src/MapPanel.java
+++ src/MapPanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 /**
  * Created by Cathal on 09/02/16.
@@ -12,4 +13,22 @@ public class MapPanel extends JPanel {
         setBackground(Color.CYAN);
 
     }
+
+//    private void paintComponent(Graphics g){
+//        super.paintComponent(g);
+//        Graphics2D g2d = (Graphics2D)g;
+//
+//        for(int i = 0; i < Main.countries.length; i++){
+//            Ellipse2D circle = new Ellipse2D.Float();
+//            circle.setFrameFromCenter(
+//
+//            );
+//        }
+//        g2d.setColor(Constants.CONTINTENT_COLORS[country.continent]);
+//        g2d.fillOval((int)country.coOrds.getX(), (int)country.coOrds.getY(), 10, 10);
+//    }
+//
+//    private void paintAllCountries()
+
+
 }
