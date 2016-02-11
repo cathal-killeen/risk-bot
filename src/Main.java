83d9b2cdae73b3f8de470fe69188fdea00857bb2
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-11 22:45:54 +0000

    Moved createPlayers to Player class

--- src/Main.java
+++ src/Main.java
@@ -32,62 +32,8 @@ public class Main {
     }
 
     public static void init(){
-        players = createPlayers();
+        players = Player.createPlayers();
         gameFrame = new GameFrame();
-    }
-
-
-
-
-    public static ArrayList createPlayers(){
-        ArrayList<Integer> availableCountries = new ArrayList<>();
-        for(int i=0; i < countries.size(); i++){
-            availableCountries.add(i);
-        }
-        ArrayList<Player> players = new ArrayList<>();
-
-        String[] playerNames = getPlayerNames();
-
-
-
-        for(int i = 0; i < 6; i++){
-            if(i >= 0 && i <= 1){
-                players.add(new Player(playerNames[i], i));
-                availableCountries = players.get(i).initialTerritories(availableCountries, 9);
-            }else{
-                players.add(new Player(playerNames[i], i));
-                availableCountries = players.get(i).initialTerritories(availableCountries, 6);
-            }
-        }
-
-        return players;
-    }
-
-    public static String[] getPlayerNames(){
-        JTextField field1 = new JTextField();
-        JTextField field2 = new JTextField();
-
-        String[] playerNames = {
-                "Player 6",
-                "Player 5",
-                "Player 4",
-                "Player 3",
-                "Player 2",
-                "Player 1"
-        };
-
-        Object[] message = {
-                "Player 1:", field1,
-                "Player 2:", field2,
-
-        };
-        int option = JOptionPane.showConfirmDialog(null, message, "Player names", JOptionPane.OK_CANCEL_OPTION);
-        if (option == JOptionPane.OK_OPTION){
-            playerNames[0] = field1.getText();
-            playerNames[1] = field2.getText();
-        }
-
-        return playerNames;
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -30,6 +31,57 @@ public class Player {
         }
 
         return availableCountries;
+    }
+
+    public static ArrayList createPlayers(){
+        ArrayList<Integer> availableCountries = new ArrayList<>();
+        for(int i=0; i < Main.countries.size(); i++){
+            availableCountries.add(i);
+        }
+        ArrayList<Player> players = new ArrayList<>();
+
+        String[] playerNames = getPlayerNames();
+
+
+
+        for(int i = 0; i < 6; i++){
+            if(i >= 0 && i <= 1){
+                players.add(new Player(playerNames[i], i));
+                availableCountries = players.get(i).initialTerritories(availableCountries, 9);
+            }else{
+                players.add(new Player(playerNames[i], i));
+                availableCountries = players.get(i).initialTerritories(availableCountries, 6);
+            }
+        }
+
+        return players;
+    }
+
+    public static String[] getPlayerNames(){
+        JTextField field1 = new JTextField();
+        JTextField field2 = new JTextField();
+
+        String[] playerNames = {
+                "Player 6",
+                "Player 5",
+                "Player 1",
+                "Player 2",
+                "Player 3",
+                "Player 4"
+        };
+
+        Object[] message = {
+                "Player 1:", field1,
+                "Player 2:", field2,
+
+        };
+        int option = JOptionPane.showConfirmDialog(null, message, "Player names", JOptionPane.OK_CANCEL_OPTION);
+        if (option == JOptionPane.OK_OPTION){
+            playerNames[0] = field1.getText();
+            playerNames[1] = field2.getText();
+        }
+
+        return playerNames;
     }
 
 
