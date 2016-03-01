a1a795d06961f6a47529317a7973e50f1cd73931
Adam <adam.doran@ucdconnect.ie>
2016-03-01 18:09:35 +0000

    Colored commands and added Commands.java

--- src/SideBar.java
+++ src/SideBar.java
@@ -3,16 +3,20 @@ import com.sun.tools.internal.jxc.ap.Const;
 import sun.tools.jconsole.BorderedComponent;
 
 import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.text.html.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+
 
 /**
  * Created by Cathal on 09/02/16.
  */
 public class SideBar extends JPanel {
     public JScrollPane ChatBoxScrollPane;
-    public JTextArea ChatBox = new JTextArea();
+    public JTextPane ChatBox = new JTextPane();
     public JTextField CommandField = new JTextField();
+    public StyledDocument styledDoc = ChatBox.getStyledDocument();
 
     public SideBar() {
         super();
@@ -20,7 +24,6 @@ public class SideBar extends JPanel {
         setLayout(new BorderLayout());
 
         ChatBox.setEditable(false);
-        ChatBox.setLineWrap(true);
         ChatBoxScrollPane = new JScrollPane(ChatBox);
         ChatBoxScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         ChatBoxScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -35,11 +38,18 @@ public class SideBar extends JPanel {
     private Action enterKeyPressed = new AbstractAction(){
         @Override
         public void actionPerformed(ActionEvent e){
-            Main.countries.get(0).addTroops(10);
-            String command = CommandField.getText();
-            ChatBox.append(command + "\n");
+            //Main.countries.get(0).addTroops(10);
+
+
+            String command = CommandField.getText() + "\n";
+            //ChatBox.setText(ChatBox.getText() + command);
+            Style style = ChatBox.addStyle("I'm a Style", null);
+            StyleConstants.setForeground(style, Color.red);
+            //example style color red 
+            try { styledDoc.insertString(styledDoc.getLength(), command, style); }
+            catch (BadLocationException ex){}
+
             CommandField.setText("");
-            System.out.println(CommandField.getText());
         }
     };
 }
