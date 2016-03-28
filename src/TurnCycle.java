66015979ae4c918841e61d4d08e7637175bb86ac
adam-doran <adam.doran@ucdconnect.ie>
2016-03-29 00:25:10 +0100

    finished attack prep, just dice rolling & troop modifiers left

--- src/TurnCycle.java
+++ src/TurnCycle.java
@@ -87,11 +87,12 @@ public class TurnCycle {
     public static void attackSequence(){
         String input;
         int x;
+        int y;
         while ((input = GameFrame.SideBar.getCommand().toLowerCase()) != "skip"){
             GameFrame.SideBar.log("Please enter a territory name you wish to attack from.\n", GameFrame.SideBar.prompt);
             String attacker = GameFrame.SideBar.getCommand();
             //check if input is a country
-            if ((x=Commands.isCountry(input)) >= 0) {
+            if ((x=Commands.isCountry(attacker)) >= 0) {
                 //check if player owns the country
                 if (Country.countries.get(x).getOwner().index == Player.currentPlayer) {
                     //check if country has sufficient troops
@@ -100,14 +101,32 @@ public class TurnCycle {
                         GameFrame.SideBar.log("Please enter a territory name you wish to attack from.\n", GameFrame.SideBar.prompt);
                         String attackee = GameFrame.SideBar.getCommand();
                         //check is attackee a country
-                        if ((x=Commands.isCountry(input)) >= 0) {
+                        if ((y=Commands.isCountry(attackee)) >= 0) {
                             //check if attacker owns attackee
-                            if (Country.countries.get(x).getOwner().index != Player.currentPlayer) {
+                            if (Country.countries.get(y).getOwner().index != Player.currentPlayer) {
+                                boolean isAdjacent = false;
+                                //check are countries adjacent
+                                for (int i=0; i< Constants.ADJACENT.length; i++) {
+                                    if (Country.countries.get(y).index == Constants.ADJACENT[x][i]){
+                                        isAdjacent = true;
+                                    }
+                                }
+                                //if adjacent
+                                if (isAdjacent){
+                                    GameFrame.SideBar.log("How many troops would you like to attack with?.\n", GameFrame.SideBar.prompt);
+                                    int numTroops = Integer.parseInt(GameFrame.SideBar.getCommand());
 
-                                
+                                    //check if the number of troops is valid
+                                    if (numTroops > 1 && numTroops <= 3 && numTroops >= Country.countries.get(x).getTroopCount()){
 
+                                        //DO ATTACK STUFF HERE
 
-
+                                    } else {
+                                        GameFrame.SideBar.log("That is an invalid number of troops to attack with. Please try again.\n", GameFrame.SideBar.error);
+                                    }
+                                } else {
+                                    GameFrame.SideBar.log("These territories are not adjacent. Please select two territories that are.\n", GameFrame.SideBar.error);
+                                }
                             } else {
                                 GameFrame.SideBar.log("You own this territory. Please select one that you do not currently control.\n", GameFrame.SideBar.error);
                             }
@@ -125,5 +144,10 @@ public class TurnCycle {
             }
         }
     }
+
+    public static void fortify(){
+        //figure out way to check if there exists an link between two chosen territories
+    }
+
 
 }
