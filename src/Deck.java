68eb4389d2ae210dc5af0aa56e36d52b7137198d
adam-doran <adam.doran@ucdconnect.ie>
2016-04-10 16:22:21 +0100

    Added Deck.java with subclass Card. Creates & shuffles deck

---
+++ src/Deck.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by adam on 10/04/2016.
+ */
+public class Deck {
+    class Card{
+        public String country;
+        public String insignia;
+
+        public Card(String c, String i){
+            this.country = c;
+            this.insignia = i;
+        }
+    }
+
+    private ArrayList<Card> deck = new ArrayList<Card>(44);
+
+    public Deck(){
+        String insignia;
+        for (int i=0; i<44; i++){
+            if (i%3 == 0){
+                insignia = "Infantry";
+            } else if (i%3 == 1){
+                insignia = "Cavalry";
+            } else {
+                insignia = "Artilery";
+            }
+            if (i >= 42){
+                deck.add(new Card("Wild", "Wild"));
+            } else {
+                deck.add(new Card(Constants.COUNTRY_NAMES[i], insignia));
+            }
+        }
+
+        Collections.shuffle(deck);
+    }
+}
