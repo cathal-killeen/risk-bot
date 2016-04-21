dd1d78d6f42cb2ca2f3064d495d0ead594acd067
Cathal Killeen <cathalkilleen@gmail.com>
2016-04-21 21:35:13 +0100

    added country subclass

--- src/SuckyBeigeFish.java
+++ src/SuckyBeigeFish.java
@@ -1,5 +1,7 @@
 // put your code here
 
+import java.util.ArrayList;
+
 public class SuckyBeigeFish implements Bot {
 	// The public API of YourTeamName must not change
 	// You cannot change any other classes
@@ -9,11 +11,13 @@ public class SuckyBeigeFish implements Bot {
 	
 	private BoardAPI board;
 	private PlayerAPI player;
+    private ArrayList<Country> countries;
 
 	SuckyBeigeFish(BoardAPI inBoard, PlayerAPI inPlayer) {
 		board = inBoard;	
 		player = inPlayer;
 		// put your code here
+        countries = createCountriesList();
 		return;
 	}
 	
@@ -76,4 +80,109 @@ public class SuckyBeigeFish implements Bot {
 		return(command);
 	}
 
+    //creates a list of all the countries on the board - country data can then be easily accessed by using 'countries.get(countryId)'
+    private ArrayList<Country> createCountriesList(){
+        ArrayList<Country> list = new ArrayList<>();
+        for(int i=0; i<GameData.NUM_COUNTRIES; i++){
+           list.add(new Country(i));
+        }
+        return list;
+    }
+
+
+
+    //internal country class for storing and retrieving info about ALL countries
+    class Country{
+        public int index;
+        public String name;
+        public int continent;
+        public int[] adjacents;
+
+        public Country(int ind){
+            index = ind;
+            name = GameData.COUNTRY_NAMES[index];
+            continent = GameData.CONTINENT_IDS[index];
+            adjacents = GameData.ADJACENT[index];
+        }
+
+        public int owner(){
+            return board.getOccupier(index);
+        }
+
+        public Boolean hasOwner(){
+            return board.isOccupied(index);
+        }
+
+        public int numUnits(){
+            return board.getNumUnits(index);
+        }
+
+        public ArrayList<Country> getOwnedAdjacents(){
+            return getOwnedAdjacents(new ArrayList<Country>());
+        }
+
+        //get the neighboring countries owned by the same person as this country
+        private ArrayList<Country> getOwnedAdjacents(ArrayList<Country> excludeList){
+            ArrayList<Country> adjacentsList = new ArrayList<>();
+            for(int i=0; i<adjacents.length; i++){
+                Country adj = countries.get(adjacents[i]);
+                Boolean excl = false;
+                for(Country excluded: excludeList){
+                    if(excluded == adj){
+                        excl = true;
+                    }
+                }
+                if(adj.owner() == owner() && excl == false){
+                    adjacentsList.add(adj);
+                }
+            }
+            return adjacentsList;
+        }
+
+        public ArrayList<Country> getCountryGroup(){
+            return getCountryGroup(new ArrayList<Country>());
+        }
+
+        //get the group of countries that this country belongs to - ie all of the countries connected and owned by the same player
+        private ArrayList<Country> getCountryGroup(ArrayList<Country> excludeList){
+            ArrayList<Country> thisCountry = new ArrayList<>();
+            thisCountry.add(this);
+
+            ArrayList<Country> countryGroup = mergeCountryGroups(new ArrayList<Country>(), thisCountry);
+
+            ArrayList<Country> ownedAdjacents = getOwnedAdjacents(excludeList);
+            if(ownedAdjacents.size() > 0){
+                countryGroup = mergeCountryGroups(countryGroup, ownedAdjacents);
+                for(Country adj: ownedAdjacents){
+                    countryGroup = mergeCountryGroups(countryGroup, adj.getCountryGroup(countryGroup));
+                }
+            }
+
+            return countryGroup;
+        }
+
+        //merges two arraylists of countries together - ensuring duplicates are removed
+        private ArrayList<Country> mergeCountryGroups(ArrayList<Country> g1, ArrayList<Country> g2){
+            for(Country member: g2){
+                //if g1 doesnt already contain member of g2
+                if(!groupHasCountry(g1,member)){
+                    g1.add(member);
+                }
+            }
+
+            return g1;
+        }
+
+        //check if group of contries contains given country
+        public Boolean groupHasCountry(ArrayList<Country> group, Country country){
+            for(Country member: group){
+                if(member == country){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
+
